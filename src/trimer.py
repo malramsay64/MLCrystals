8f838ce7ca260785f036812f19b567f13394b0c7
Malcolm Ramsay <malramsay64@gmail.com>
2018-12-06 09:36:08 +1300

    DOC Finalising SM2 talk including images
    This puts finishing touches on my talk for SM2, including the
    supplementary jupyter notebook and images included in the presentation.

--- src/trimer.py
+++ src/trimer.py
@@ -8,11 +8,12 @@
 
 """Utilities for handling the trimer molecule."""
 
-from itertools import product, count
+from itertools import count, product
 from pathlib import Path
 from typing import List
 
 import gsd.hoomd
+import matplotlib.pyplot as plt
 import numpy as np
 from bokeh.plotting import gridplot
 from scipy.sparse import coo_matrix
@@ -107,3 +108,39 @@ def neighbour_connectivity(snapshot, max_neighbours=6, max_radius=5):
     )
     connectivity = coo_matrix((sparse_values, sparse_coordinates))
     return connectivity.toarray()
+
+
+def plot_confusion_matrix(
+    cm, classes, normalize=True, title="Confusion matrix", cmap=plt.cm.Blues
+):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print("Confusion matrix, without normalization")
+
+    plt.imshow(cm, interpolation="nearest", cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = ".2f" if normalize else "d"
+    thresh = cm.max() / 2.0
+    for i, j in product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(
+            j,
+            i,
+            format(cm[i, j], fmt),
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black",
+        )
+
+    plt.tight_layout()
+    plt.ylabel("True label")
+    plt.xlabel("Predicted label")
