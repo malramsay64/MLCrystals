fc701322d2bdbe6a930ae7a6779debd165fe9a9c
Malcolm Ramsay <malramsay64@gmail.com>
2019-05-30 10:26:36 +1000

    figure: Split up figures into smaller functions
    This breaks up the code for the figures, turning it into a collection of
    smaller functions which can additionally be used in the jupyter
    notebooks.

--- src/figures.py
+++ src/figures.py
@@ -6,48 +6,49 @@
 #
 # Distributed under terms of the MIT license.
 
-"""
+"""A collection of tools to generate figures.
+
+This module contains functions for the creation of figures, both through a jupyter
+notebook interface or on the command line.
 
 """
+
+from typing import List, Tuple
 
 import click
 import gsd.hoomd
 from bokeh.io import export_png
+from bokeh.plotting import Figure
 from sdanalysis import HoomdFrame
 from sdanalysis.figures import plot_frame
 
 
-@click.group()
-def main():
-    pass
+def cell_regions(
+    x_len: float, y_len: float, factor: float = 2 / 3, buffer: float = 3.5
+) -> Tuple[List[List[float]], ...]:
+    """Calculate the boundary of the different regions used for classification.
 
+    When running an interface simulation there is a region in which the molecules are
+    not integrated, being the internal crystal region, while all the surrounding
+    material is melted. This calculates the boundaries of that region from the length of
+    the x and y coordinates of the simulation cell.
 
-@main.command()
-@click.argument("infile", type=click.Path(exists=True, dir_okay=False))
-@click.option(
-    "-i",
-    "--index",
-    default=0,
-    help="The index of the frame within the input file to plot",
-    type=int,
-)
-def labelled_config(infile, index):
-    """Plot an input configuration indicating the labelling scheme.
+    Args:
+        x_len: The length of the x component of the simulation cell
+        y_len: The length of the y component of the simulations cell
+        factor: The fraction of each side of the box which remains crystalline
+        buffer: Size of the buffer region around the interface. This is to help
+            the machine learning algorithm by not introducing molecular
+            configurations which are confusing and ill defined.
 
-    This plots the configuration with an overlay indicating the regions in which
-    particles are classified as liquid (blue) and the regions where they are classified
-    as crystal (red).
+    Returns:
+        cell: The coordinates of the complete simulation cell
+        liq: The inner boundary of the liquid region
+        crys: The outer boundary of the crystal region
 
     """
-    with gsd.hoomd.open(infile) as trj:
-        snap = HoomdFrame(trj[index])
-
-    x_len, y_len = snap.box[:2]
     x_min, x_max = x_len / 2, -x_len / 2
     y_min, y_max = y_len / 2, -y_len / 2
-
-    factor = 2 / 3
-    buffer = 3.5
 
     cell = [[x_min, x_max, x_max, x_min], [y_min, y_min, y_max, y_max]]
 
@@ -81,7 +82,46 @@ def labelled_config(infile, index):
         ],
     ]
 
-    fig = plot_frame(snap)
+    return cell, liq, crys
+
+
+def style_snapshot(figure: Figure) -> Figure:
+    """Style a bokeh figure as a configuration snapshot.
+
+    This is collection of style changes to make the output of a snapshot consistent and
+    nice. Primarily it removes all the extra stuff which isn't helpful in defining the
+    configuration like the axes, and the interactive tools.
+
+    """
+    figure.axis.visible = False
+    figure.xgrid.visible = False
+    figure.ygrid.visible = False
+    figure.toolbar_location = None
+    figure.toolbar.logo = None
+
+    return figure
+
+
+@click.group()
+def main():
+    pass
+
+
+def plot_labelled_config(snapshot: HoomdFrame) -> Figure:
+    """Plot an input configuration indicating the labelling scheme.
+
+    This plots the configuration with an overlay indicating the regions in which
+    particles are classified as liquid (blue) and the regions where they are classified
+    as crystal (red).
+
+    Args:
+        snapshot: The snapshot configuration to display
+
+    """
+    x_len, y_len = snapshot.box[:2]
+    cell, liq, crys = cell_regions(x_len, y_len)
+
+    fig = plot_frame(snapshot)
     # use the canvas backend which supports transparency and other effects.
     fig.output_backend = "canvas"
 
@@ -103,11 +143,30 @@ def labelled_config(infile, index):
         line_color=None,
     )
 
-    fig.axis.visible = False
-    fig.xgrid.visible = False
-    fig.ygrid.visible = False
-    fig.toolbar_location = None
-    fig.toolbar.logo = None
+    return style_snapshot(fig)
+
+
+@main.command()
+@click.argument("infile", type=click.Path(exists=True, dir_okay=False))
+@click.option(
+    "-i",
+    "--index",
+    default=0,
+    help="The index of the frame within the input file to plot",
+    type=int,
+)
+def labelled_config(infile, index):
+    """Plot an input configuration indicating the labelling scheme.
+
+    This plots the configuration with an overlay indicating the regions in which
+    particles are classified as liquid (blue) and the regions where they are classified
+    as crystal (red).
+
+    """
+    with gsd.hoomd.open(infile) as trj:
+        snap = HoomdFrame(trj[index])
+
+    fig = plot_labelled_config(snap)
 
     export_png(fig, "figures/labelled_config.png", height=1600, width=3200)
 
